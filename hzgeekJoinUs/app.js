b7815e99592f5b39c9e83bf008d6491720feb7ba
JupiterJun <hbbdmjz@163.com>
2021-01-21 12:41:19 +0800

    最后更新

--- hzgeekJoinUs/app.js
+++ hzgeekJoinUs/app.js
@@ -6,7 +6,7 @@ for (var i of $(".fadein")) {
 
     })(i);
 }
-particlesJS.load('particles-js', './particles.json', function() {});
+particlesJS.load('particles-js', '//cdn.jsdelivr.net/gh/JupiterJun/cdn/hzgeekJoinUs/particles.min.json', function() {});
 btn = $("button")[0]
 btn.onclick = () => { window.location.href = "https://cdn.jsdelivr.net/gh/JupiterJun/picture/JupiterJun's_QQ.jpg" }
 btn.onmouseover = () => { btn.classList.add('shadow'); }
