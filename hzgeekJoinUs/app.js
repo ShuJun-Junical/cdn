f228b8872f4be052d1d55944c6ee35a4753029b5
青章浚 <39018081+JupiterJun@users.noreply.github.com>
2021-06-06 21:40:07 +0800

    Update app.js

--- hzgeekJoinUs/app.js
+++ hzgeekJoinUs/app.js
@@ -6,8 +6,8 @@ for (var i of $(".fadein")) {
 
     })(i);
 }
-particlesJS.load('particles-js', '//cdn.jsdelivr.net/gh/JupiterJun/cdn/hzgeekJoinUs/particles.min.json', function() {});
+particlesJS.load('particles-js', '//cdn.jsdelivr.net/gh/JupiterJun/cdn/hzgeekJoinUs@main/particles.min.json', function() {});
 btn = $("button")[0]
-btn.onclick = () => { window.location.href = "https://cdn.jsdelivr.net/gh/JupiterJun/picture/JupiterJun's_QQ.jpg" }
+btn.onclick = () => { window.location.href = "https://cdn.jsdelivr.net/gh/JupiterJun/picture@main/JupiterJun's_QQ.jpg" }
 btn.onmouseover = () => { btn.classList.add('shadow'); }
 btn.onmouseout = () => { btn.classList.remove('shadow'); }
